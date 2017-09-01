5b29f341c56e831af8e8968dc6b41391a6b93843
Owen Essen <abc_abcwebd@yahoo.com>
2017-08-31 20:25:44 -0400

    Vague pseudo coding.

---
+++ models/user.js
@@ -0,0 +1,6 @@
+/*
+Tutorial on setting up authentication with Sequelize and Passport: 
+https://code.tutsplus.com/tutorials/using-passport-with-sequelize-and-mysql--cms-27537
+
+You all okay with using Passport?
+*/
---
+++ routes/html-routes.js
@@ -0,0 +1,19 @@
+/*
+Will need: 
+
+Homepage
+   User can read about site
+   User can upload photo
+   User can sign in or access profile
+
+Sign In/Sign Up
+
+Result page (after photo upload)
+	Could also be modal on homepage (something like a single page app)
+
+Profile page
+	All matches a user has ever uploaded. 
+
+
+Other pages we need to have?
+*/
