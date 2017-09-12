a92189927fcbcbd4b46e7aad074881ddcf888858
Alec Schleicher <schleicher@3cisd.com>
2017-09-12 11:18:32 -0400

    api key change

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,7 @@
 module.exports = (app) => {
 	app.get("/v1", (req, res) => {
-		res.render("test");
+		var subscriptionKey = "13hc77781f7e4b19b5fcdd72a8df7156";
+		var uriBase = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect";
+		res.render("index");
 	})
 };
