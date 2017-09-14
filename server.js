cca6ea8fb1df039e298682cace07a54acb7b5718
Alec Schleicher <schleicher@3cisd.com>
2017-09-14 13:30:26 -0400

    updated config

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || process.argv[3] || 80;
 
 const passport = require('passport');
 const session = require('express-session');
